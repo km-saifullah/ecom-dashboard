217000b6679b684d6d7b0743030d39ffc7faa58d
Md. Masrafi Mondol <masrafisw@gmail.com>
2024-11-07 16:09:25 +0600

    profile section changes

--- src/pages/layout/Navbar.jsx
+++ src/pages/layout/Navbar.jsx
@@ -3,12 +3,21 @@ import { IoSearchOutline } from "react-icons/io5";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoIosArrowForward, IoMdNotificationsOutline } from "react-icons/io";
 import Image from "../../components/Image";
-import avatar from "/vite.svg";
+import avatar from "/randoProfileImg.jpeg";
+import { useState } from "react";
 
 const Navbar = ({ setIsOpen, isOpen }) => {
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
+  // profile dropdown
+  const [isOpenProfile, setIsOpenProfile] = useState(false);
+
+  const toogleDropdown = () => {
+    setIsOpenProfile(!isOpenProfile);
+  };
+
   return (
     <nav className="py-4 flex items-center justify-between">
       <section className="flex items-center gap-x-7">
@@ -16,11 +25,7 @@ const Navbar = ({ setIsOpen, isOpen }) => {
           <HiOutlineBars3CenterLeft className="w-[35px] h-[35px] text-primary cursor-pointer" />
         </div>
         <div className="flex items-center justify-between w-[300px] h-[52px]">
-          <input
-            type="text"
-            className=""
-            placeholder="Search.."
-          />
+          <input type="text" className="" placeholder="Search.." />
           <IoSearchOutline className="w-[35px] h-[35px] text-text cursor-pointer" />
         </div>
       </section>
@@ -31,12 +36,34 @@ const Navbar = ({ setIsOpen, isOpen }) => {
         <div className="cursor-pointer">
           <IoMdNotificationsOutline className="text-primary text-2xl" />
         </div>
-        <div className="cursor-pointer">
-          <Image imgSrc={avatar} imgAlt="avatar image not found" />
-        </div>
-        <div className="flex items-center justify-between">
-          <h4>John Doe</h4>
-          <IoIosArrowForward className="" />
+
+        {/*  userProfile start */}
+        <div className="flex items-center gap-3" onClick={toogleDropdown}>
+          <div>
+            <img
+              className="cursor-pointer h-[40px] w-[40px] rounded-full "
+              src="/randoProfileImg.jpeg"  //this image source will come from backend. 
+              alt="user profile image"
+            />
+          </div>
+          <div className="relative inline-block">
+            <button className="text-sm font-medium text-base hover:text-gray-900">
+              John Doe
+            </button> {/* the name will come from backend. test dummy. */}
+
+            {isOpenProfile && (
+              <div className="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md">
+                <ul>
+                  <li className="py-2 px-4 hover:bg-gray-100">
+                    <a href="#">See Profile</a>
+                  </li>
+                  <li className="py-2 px-4 hover:bg-gray-100">
+                    <a href="#">Logout</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </section>
     </nav>
