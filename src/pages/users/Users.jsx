fc58002268660284f730c049a46f181ad14da327
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-13 13:05:42 +0600

    updated user table

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -25,7 +25,9 @@ const Users = () => {
     <section className="mx-6">
       {/* filter system */}
       <section className="mb-12 ">
-        <h2 className="text-3xl capitalize font-medium py-3 text-primary  ">filter</h2>
+        <h2 className="text-3xl capitalize font-medium py-3 text-primary  ">
+          filter
+        </h2>
         <div className="flex items-center justify-between ">
           <input
             className="border px-2 py-2 rounded-md w-[20rem] "
@@ -67,7 +69,9 @@ const Users = () => {
       {/* user List */}
       <section>
         <div className="border-b-2 outline-offset-8 py-3 flex justify-between items-center ">
-          <h2 className="text-3xl capitalize font-medium py-3 text-primary  ">Users</h2>
+          <h2 className="text-3xl capitalize font-medium py-3 text-primary  ">
+            Users
+          </h2>
           <div className="flex items-center gap-3">
             <button>delete</button>
             <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -77,37 +81,179 @@ const Users = () => {
           </div>
         </div>
         <div>
-          <table className="w-full border-collapse">
-            <thead >
-              <tr className=" border-b-2 outline-offset-8 ">
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Activity</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Khalid saifullah</td>
-                <td>Khalid@some.com</td>
-                <td>017********5</td>
-                <td> active</td>
-
-                <td className="flex justify-end gap-3 ">
-                  <button className="bg-black hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-                    Edit
-                  </button>
-                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <th>Name</th>
+                  <th>Job</th>
+                  <th>Favorite Color</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">Hart Hagerty</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    Zemlak, Daniel and Leannon
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      Desktop Support Technician
+                    </span>
+                  </td>
+                  <td>Purple</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+                {/* row 2 */}
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/3@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">Brice Swyre</div>
+                        <div className="text-sm opacity-50">China</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    Carroll Group
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      Tax Accountant
+                    </span>
+                  </td>
+                  <td>Red</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+                {/* row 3 */}
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/4@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">Marjy Ferencz</div>
+                        <div className="text-sm opacity-50">Russia</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    Rowe-Schoen
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      Office Assistant I
+                    </span>
+                  </td>
+                  <td>Crimson</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+                {/* row 4 */}
+                <tr>
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/5@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">Yancy Tear</div>
+                        <div className="text-sm opacity-50">Brazil</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    Wyman-Ledner
+                    <br />
+                    <span className="badge badge-ghost badge-sm">
+                      Community Outreach Specialist
+                    </span>
+                  </td>
+                  <td>Indigo</td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                  </th>
+                </tr>
+              </tbody>
+              {/* foot */}
+              <tfoot>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Job</th>
+                  <th>Favorite Color</th>
+                  <th></th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
         </div>
       </section>
-    </section >
+    </section>
   );
 };
 
